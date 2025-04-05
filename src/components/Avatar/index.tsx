49533a59a4667542f23ee08d540c3cbd31c9b555
Joao Victor Angelo <146788838+JoaoVictorAngelo@users.noreply.github.com>
2025-04-05 20:41:46 -0300

    estilizando a Sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/JoaoVictorAngelo.png" />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/JoaoVictorAngelo.png" alt="Foto de Pergil" />
-    <Titulo fontSize={20}>João Ângelo</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>João Ângelo</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        JoaoVictorAngelo
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
