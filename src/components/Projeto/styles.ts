c3d939cb40e1c144456dfb4a4ca2d5e013a1f08f
Joao Victor Angelo <146788838+JoaoVictorAngelo@users.noreply.github.com>
2025-04-05 20:10:59 -0300

    criando a seção de projetos e o componente de projetos

---
+++ src/components/Projeto/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Card = styled.div`
+  border: 1px solid #c1c1c1;
+  padding: 16px;
+`
+export const LinkBotao = styled.a`
+  display: inline-block;
+  padding: 8px;
+  color: #fff;
+  font-size: 14px;
+  background-color: #4776bf;
+  text-decoration: none;
+  margin-top: 24px;
+`
