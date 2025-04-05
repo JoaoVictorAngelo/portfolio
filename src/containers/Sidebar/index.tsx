43358139e82d670984cf4498b8f988a523e1b177
Joao Victor Angelo <146788838+JoaoVictorAngelo@users.noreply.github.com>
2025-04-05 19:38:29 -0300

    construindo a estrutura e a seção  'sobre mim'

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/JoaoVictorAngelo.png" alt="Foto de Pergil" />
+    <Titulo fontSize={20}>João Ângelo</Titulo>
+  </aside>
+)
+
+export default Sidebar
